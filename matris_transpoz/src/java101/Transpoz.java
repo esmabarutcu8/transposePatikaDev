fff9c5c2d0a1b19804eaab244779b67af2cb1899
esmabarutcu8 <80422181+esmabarutcu8@users.noreply.github.com>
2022-03-19 23:40:49 +0300

    java_odev

---
+++ matris_transpoz/src/java101/Transpoz.java
@@ -0,0 +1,30 @@
+package java101;
+
+public class Transpoz {
+
+    public static void main(String[] args) {
+
+            int a[][]={{1,2,3},{4,5,6},{7,8,9}};
+            int b[][] = new int[3][3];
+            System.out.println("Matrix : ");
+
+            for(int i = 0;i<3;i++){
+                for(int j = 0;j<3;j++){
+                    System.out.print(a[i][j]+" ");
+                }
+                System.out.println();
+            }
+            System.out.println("Transpose: ");
+
+            for(int i = 0;i<3;i++){
+                for(int j = 0;j<3;j++){
+                    b[i][j] = 0;
+                    for(int k = 0;k<3;k++){
+                        b[i][j]=a[j][i];
+                    }
+                    System.out.print(b[i][j]+" ");
+                }
+                System.out.println();
+            }
+        }
+    }
